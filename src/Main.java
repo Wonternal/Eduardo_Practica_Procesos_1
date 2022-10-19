a50b389f2382420894840c1012e52d42b8d31971
Eduardo Camacho Díaz <fgada200@gmail.com>
2022-10-19 13:29:29 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * 1.- Desarrolla una aplicación en Java que abra el número de ventanas del navegador de tu elección
+         * que indique el usuario. Puede indicarlo como entrada de teclado o como argumento del ejecutable.
+         * El proceso debe terminar cuando todas las ventanas del navegador se cierren. (3 puntos)
+         */
+
+        // Página con información relacionada al ejercicio. (Fuentes)
+        // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/ProcessBuilder.html
+        // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Process.html
+
+    }
+}
